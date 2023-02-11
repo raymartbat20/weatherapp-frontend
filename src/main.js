e1e26b20431cf37aafa39b43b1be69d6da780715
Raymart Palermo <raymartbat20@gmail.com>
2023-02-11 20:13:55 +0800

    landing page and implementation of auth0

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { createAuth0 } from '@auth0/auth0-vue';
 
 import './assets/main.css'
 
 const app = createApp(App)
 
-app.use(router)
+// app.use(router)
+app.use(
+    createAuth0({
+        domain: "dev-fo5konvpkt4e4gxk.us.auth0.com",
+        clientId: "SPtsnzY8HRxTj7PvXyJpPJ6hyvQXsa5X",
+        authorizationParams: {
+        redirect_uri: window.location.origin
+        }
+    })
+);
 
 app.mount('#app')
