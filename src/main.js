5f3f2062e247df8a3812849da319feb12c7998f0
Raymart Palermo <raymartbat20@gmail.com>
2023-02-11 22:48:39 +0800

    create-input-component

--- src/main.js
+++ src/main.js
@@ -4,11 +4,24 @@ import { createAuth0 } from '@auth0/auth0-vue';
 import './assets/main.css'
 
 import Button from './components/general/Button.vue';
+import Input from './components/general/Input.vue';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import all icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas);
 const app = createApp(App)
 
 // app.use(router)
 app.component('Button',Button);
+app.component('Input',Input);
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.use(
     createAuth0({
         domain: import.meta.env.VITE_AUTH0_DOMAIN,
